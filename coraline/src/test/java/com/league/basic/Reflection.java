3b3d756ba6f5ed612f1e880e33ea7446e06c8b12
liyun12 <liyun12@meituan.com>
2019-05-21 20:14:30 +0800

    modify reflection

--- coraline/src/test/java/com/league/basic/Reflection.java
+++ coraline/src/test/java/com/league/basic/Reflection.java
@@ -4,4 +4,10 @@ package com.league.basic;
  * Created by coraline on 2019/5/13.
  */
 public class Reflection {
+
+    public static void main(String[] args) throws ClassNotFoundException, IllegalAccessException, InstantiationException {
+        Class<?> className = Class.forName(Reflection.class.getName());
+        System.out.println(className.newInstance().toString());
+
+    }
 }
