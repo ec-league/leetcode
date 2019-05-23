534624a68af2fc14ae6c7ac2385687bbbf9641a7
liyun12 <liyun12@meituan.com>
2019-05-23 19:36:27 +0800

    add sort

--- coraline/src/test/java/com/league/leetcode/p910_sort/QuickSort.java
+++ coraline/src/test/java/com/league/leetcode/p910_sort/QuickSort.java
@@ -6,15 +6,53 @@ package com.league.leetcode.p910_sort;
 public class QuickSort {
 
     public static int[] sortArray(int[] nums) {
-
-        return null;
+        if (nums.length < 2) {
+            return nums;
+        }
+        quickSort(nums, 0, nums.length - 1);
+        return nums;
 
     }
 
+    public static void quickSort(int[] nums, int low, int high) {
+        if(low >= high){
+            return;
+        }
+        int i = low;
+        int j = high;
+        int currentNum = nums[low];
+        int index = low;
+        while (i < j) {
+            while (i < j && (nums[j] >= currentNum)) {
+                j--;
+            }
+            if (i < j) {
+                int temp = nums[j];
+                nums[j] = nums[index];
+                nums[index] = temp;
+                index = j;
+            }
+            while (i < j && (nums[i] <= currentNum)) {
+                i++;
+            }
+            if (i < j) {
+                int temp = nums[i];
+                nums[i] = nums[index];
+                nums[index] = temp;
+                index = i;
+            }
+        }
+        quickSort(nums, low, index - 1);
+        quickSort(nums, index + 1, high);
+    }
 
 
     public static void main(String[] args) {
-
+        int[] nums = {2, 3, 1, 1};
+        int[] arr = sortArray(nums);
+        for (int i = 0; i < nums.length; i++) {
+            System.out.println(nums[i]);
+        }
     }
 
 }
